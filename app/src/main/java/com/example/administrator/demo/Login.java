b17998ce62814927d63446569be4fd4a10495b28
Administrator <850277517@qq.com>
2017-10-17 18:21:34 +0800

    login

---
+++ app/src/main/java/com/example/administrator/demo/Login.java
@@ -0,0 +1,18 @@
+package com.example.administrator.demo;
+
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/10/17.
+ */
+
+public class Login extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
