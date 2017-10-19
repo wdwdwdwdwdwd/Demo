4da81176bf3dd262d6e74e5113fafb0680594959
wd <850277517@qq.com>
2017-10-19 11:38:00 +0800

    主页面

--- app/src/main/java/com/example/administrator/demo/Login.java
+++ app/src/main/java/com/example/administrator/demo/Login.java
@@ -1,18 +1,28 @@
 package com.example.administrator.demo;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Administrator on 2017/10/17.
  */
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button login=(Button)findViewById(R.id.login_button);
+     login.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        Intent intent=new Intent(Login.this,MainActivity.class);
+        startActivity(intent);
+    }
 }
